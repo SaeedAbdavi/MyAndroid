23057a0d275334ba08d4a2a430bc9617f873a4d5
saeed <saeed.abdavi@yahoo.com>
2016-06-27 00:17:29 -0700

    saeed change

--- app/src/main/java/com/saeed/myapplication/MainActivity.java
+++ app/src/main/java/com/saeed/myapplication/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends SampleActivityBase
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("my TEST");
       //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
       //  setSupportActionBar(toolbar);
 
