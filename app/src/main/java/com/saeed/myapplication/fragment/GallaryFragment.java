d975a7131ae967549e19733738b6e1dd850f13d8
saeed <saeed.abdavi@yahoo.com>
2016-06-26 23:17:45 -0700

    this is my project

---
+++ app/src/main/java/com/saeed/myapplication/fragment/GallaryFragment.java
@@ -0,0 +1,21 @@
+package com.saeed.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.saeed.myapplication.R;
+
+/**
+ * Created by S.Abdavi on 5/25/2016.
+ */
+public class GallaryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallary_fragment,container,false);
+    }
+}
